39d661aff8775123db10a7b79dbc38b6492e0a7b
unknown <sameerstg@outlook.com>
2023-09-11 04:59:28 +0500

    chakra deleted

--- src/index.js
+++ src/index.js
@@ -2,22 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ChakraProvider ,extendTheme} from '@chakra-ui/react'
-const theme = extendTheme({
-  styles: {
-    global: () => ({
-      body: {
-        bg: "",
-      },
-    }),
-  },
-});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <ChakraProvider theme={theme}>
       <App />
-    </ChakraProvider>
   </React.StrictMode>
 );
 
