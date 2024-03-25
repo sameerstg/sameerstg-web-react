96c13cdb8620fa6fb9c43c126639c93cb2da47f0
sameerstg <sameerstg@outlook.com>
2024-03-26 00:08:19 +0500

    vercel analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Footer from "./Section/Footer";
 import Navbar from "./Section/Navbar";
-
+import { Analytics } from '@vercel/analytics/react';
 function App() {
   
   return (
@@ -16,6 +15,7 @@ function App() {
         </Routes>
         <Footer />
       </Router>
+      <Analytics />
     </div>
 
   );
