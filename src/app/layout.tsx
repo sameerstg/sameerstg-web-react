ca5ccb84c9017f488ffa8c94831bb809e495d46a
sameerstg <sameerstg@outlook.com>
2024-05-28 17:27:06 +0500

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from '../Section/Navbar'
 import Footer from '../Section/Footer'
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics mode={'production'} />;
         <Navbar />
         {children}
         <Footer />
