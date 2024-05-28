76f2dfb9ed02bdd115e2efad46639f13fc202efb
sameerstg <sameerstg@outlook.com>
2024-05-28 14:46:28 +0500

    shad cn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,12 +3,20 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{ts,tsx}",
     // "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     // "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     // "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     // "./src/Section/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     colors: {
       primary: "#ffffff",
       secondary: "#00FFFF",
@@ -27,8 +35,22 @@ const config: Config = {
         desktop: "1280px",
         // => @media (min-width: 1280px) { ... }
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
 export default config;
